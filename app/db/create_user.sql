63f3399baccd53dd2cb09bda3ad6a2f91141d691
Chandler Stolworthy <chandler.stolworthy@gmail.com>
2017-09-07 12:10:55 -0600

    second commit

--- app/db/create_user.sql
+++ app/db/create_user.sql
@@ -1,5 +1,5 @@
 INSERT INTO users
-(use_name, email, img, auth_id)
+(user_name, email, img, auth_id)
 VALUES
 ($1, $2, $3, $4)
 RETURNING *;
--- app/server/server.js
+++ app/server/server.js
@@ -27,13 +27,57 @@ massive(process.env.CONNECTIONSTRING).then( db => {
     app.set('db', db);
 })
 
-//AUTHENTICATION
+//AUTHENTICATION-AUTH0
+passport.use(new Auth0Strategy({
+    domain: process.env.AUTH_DOMAIN,
+    clientID: process.env.AUTH_CLIENT_ID,
+    clientSecret: process.env.AUTH_CLIENT_SECRET,
+    callbackURL: process.env.AUTH_CALLBACK,
+
+}, function(accessToken, refreshToken, extraParams, profile, done){
+
+    //WE ARE SETTING DB TO A VARIABLE SO THAT WE DONT NEED TO TYPE APP.GET MULTIPLE TIMES
+    const db = app.get('db');
+    
+    //WE ARE CHECKING TO SEE IF USER IS IN THE DB
+    //PASS THE USER DOWN THROUGH ALL STEPS SO THAT WE CAN PUT IT ON SESSIONS
+    db.find_user(profile.id).then( user => {
+        if(user[0]) {
+            return done(null, user);
+        } else {
+            db.create_user([profile.displayName, profile.emails[0].value, profile.picture, profile.id]).then( user => {
+                return done(null, user[0]);
+            })
+        }
+    })
+}))
+
+//THE USER IS COMING FROM THE CALLBACK FUNCTION ABOVE
+passport.serializeUser(function(user, done) {
+    done(null, user)
+})
+
+//THE USER IS COMING FROM SESSION - THIS IS INVOKED FOR EVERY ENDPOINT
+passport.deserializeUser(function(user, done){
+    app.get('db').find_session_user(user[0].id).then( user => {
+        return done(null, user[0]);
+    })
+})
+
+//ENDPOINTS
+
+//DIRECTS USER TO AUTH0.COM, LEAVING OUT SITE, FOR THE AUTHENTICATION
+app.get('/auth', passport.authenticate('auth0'));
+
+app.get('/auth/callback', passport.authenticate('auth0', {
+    successRedirect: 'http://localhost:3000/#/private',
+    failureRedirect: 'http://localhost:3000/#/'
+}))
 
 
 
 
-
-let port = 3000;
+let port = 3001;
 app.listen(port, ()=> {
     console.log(`Listening on port: ${port}`);
 })
